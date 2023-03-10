26348c0449289aa8326d0d1494e39e351ccdacf8
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-03-10 14:12:33 +0530

    Updated Resource Model

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,29 +4,28 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-9_ResourceModel.controller.View1", {
-		
-			onInit: function(){
+		onInit: function(){
 				var oModel = new sap.ui.model.json.JSONModel({
 				"lang" : "FR"
 			});
 			this.getView().setModel(oModel);
 			
-			},
+		},
+		
+		onClickEng : function(){
+			var strLang = "EN";
+			this.getView().getModel().setProperty("/lang", strLang);
+		},
+		onClickFr : function(oEvent){
+		var strLang = "FR";
+			this.getView().getModel().setProperty("/lang", strLang);
 			
-			onClickEng : function(){
-				var strLang = "EN";
-				this.getView().getModel().setProperty("/lang", strLang);
-			},
-			onClickFr : function(oEvent){
-			var strLang = "FR";
-				this.getView().getModel().setProperty("/lang", strLang);
-				
-			/*	var french = this.getOwnerComponent().getModel("i18n_fr");
-				this.getView().getModel(french);*/
-				//this.getView().byId("idName").setText(this.getOwnerComponent().getModel("i18n_fr").getResourceBundle().getText("LBL_fName"));
-				
-				
-			}
+		/*	var french = this.getOwnerComponent().getModel("i18n_fr");
+			this.getView().getModel(french);*/
+			//this.getView().byId("idName").setText(this.getOwnerComponent().getModel("i18n_fr").getResourceBundle().getText("LBL_fName"));
+			
+			
+		}
 
 	});
 });
